d503bb60e8a89a3fcf4f2888a515af647651b83e
Fahri MacBook <fahri-can@web.de>
2020-09-12 15:24:26 +0200

    Use of first Fragment.

--- app/src/main/java/com/imaginarynationalpark/view/ui/MainActivity.kt
+++ app/src/main/java/com/imaginarynationalpark/view/ui/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.fragment_container,
+                AllToursFragment()
+            )
+            .commit()
     }
 }
