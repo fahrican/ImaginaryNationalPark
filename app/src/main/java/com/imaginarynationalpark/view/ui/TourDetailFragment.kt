c79ca2a3c015ab2ae4137090184f144bb35f6aee
Fahri MacBook <fahri-can@web.de>
2020-09-12 16:31:27 +0200

    Create new detail Fragment for a tour.

---
+++ app/src/main/java/com/imaginarynationalpark/view/ui/TourDetailFragment.kt
@@ -0,0 +1,14 @@
+package com.imaginarynationalpark.view.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.imaginarynationalpark.R
+
+
+class TourDetailFragment : Fragment(R.layout.fragment_tour_detail) {
+
+
+}
