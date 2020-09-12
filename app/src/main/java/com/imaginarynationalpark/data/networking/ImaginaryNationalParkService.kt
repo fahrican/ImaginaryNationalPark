5222e23bfe42c49c0eefac0510ea52125c5d2bb7
Fahri MacBook <fahri-can@web.de>
2020-09-12 16:38:19 +0200

    Create service for endpoint request.

---
+++ app/src/main/java/com/imaginarynationalpark/data/networking/ImaginaryNationalParkService.kt
@@ -0,0 +1,32 @@
+package com.imaginarynationalpark.data.networking
+
+import com.imaginarynationalpark.data.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ImaginaryNationalParkService {
+
+    private fun createOkHttpClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+
+    fun getClient(): ImaginaryNationalParkApi {
+        val okHttpClient = createOkHttpClient()
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(ImaginaryNationalParkApi::class.java)
+    }
+}
